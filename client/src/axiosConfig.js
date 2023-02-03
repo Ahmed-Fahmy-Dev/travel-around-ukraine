026233d91dd8880fb12de21620afd6a1c7269300
Ahmed Fahmy <ahmed.fahmy.priv@gmail.com>
2023-02-03 17:22:55 +0200

    fix axios base url - base url edited to Railway deployment link

--- client/src/axiosConfig.js
+++ client/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://travel-around-ukraine-production.up.railway.app',
 });
 
 axiosConfig.interceptors.request.use(
